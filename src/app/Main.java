b605f80daa161edd32bf4a296f765e4c636dadce
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-06-14 16:43:59 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Converter App.");
+        System.out.println("App for measure converting.");
+    }
+}
